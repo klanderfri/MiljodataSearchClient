a72c1060a17cbcadb6e61f438ad0d059d0578bc8
Björn Larsson <software@bjornlarsson.dev>
2025-09-17 12:43:08 +0200

    Låter Visual Studio generera Hello World-solution.

---
+++ MiljodataSearchClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiljodataSearchClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
